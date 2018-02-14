e6b94af526b00a0c72783fdca7537c22e95d93e6
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-02-14 13:57:11 -0600

    adds data repository as component with dependency injection in controller

--- src/main/java/com/ryanwittrup/giflib/controller/GifController.java
+++ src/main/java/com/ryanwittrup/giflib/controller/GifController.java
@@ -1,11 +1,20 @@
 package com.ryanwittrup.giflib.controller;
 
+import com.ryanwittrup.giflib.data.GifRepository;
+import com.ryanwittrup.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
+
 @Controller
 public class GifController {
+
+    @Autowired // dependency injection
+    private GifRepository gifRepository;
 
     @RequestMapping("/")
     public String listGifs() {
@@ -13,8 +22,12 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String listGif() {
-        return "info for a single GIF";
+    public String gifDetails(ModelMap modelMap) {
+
+        Gif gif = gifRepository.findByName("android-explosion");
+
+        modelMap.put("gif", gif);
+
+        return "gif-details";
     }
 }
